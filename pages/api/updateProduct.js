0c592717d25c674db2b3eafb512fa7cc8f762f18
Navneet018 <navneet@logicfirst.io>
2023-05-15 03:07:57 +0530

    changes

--- pages/api/updateProduct.js
+++ pages/api/updateProduct.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
         if (req.method == "POST") {
 
             for (let i = 0; i < req.body.length; i++) {
-                 await Product.findByIdAndUpdate(req.body[i]._id,req.body[i]);
+                await Product.findByIdAndUpdate(req.body[i]._id, req.body[i]);
             }
 
             res.status(200).json({
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
             })
         }
     } catch (error) {
-        res.json({error:error})
+        res.json({ error: error })
 
     }
 
